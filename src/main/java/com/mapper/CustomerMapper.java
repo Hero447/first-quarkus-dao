e3efcd6fb289522327d749c5a025f4b30dbfb696
Hero447 <sergsoldpp@gmail.com>
2026-03-11 14:59:54 +0200

    replacing implementation of mapping with mapstruct

--- src/main/java/com/mapper/CustomerMapper.java
+++ src/main/java/com/mapper/CustomerMapper.java
@@ -2,48 +2,17 @@ package com.mapper;
 
 
 import com.domain.Customer;
-import jakarta.enterprise.context.ApplicationScoped;
+import org.mapstruct.Mapper;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
-@ApplicationScoped
-public class CustomerMapper {
-    public Customer customerToEntity(com.proto.service.Customer customer) {
-        if ( customer == null ) {
-            return null;
-        }
+@Mapper(componentModel = "jakarta")
+public interface CustomerMapper {
+    Customer customerToEntity(com.proto.service.Customer customer);
 
-        Customer customerEntity = new Customer();
-        customerEntity.setId(customer.getId());
-        customerEntity.setName(customer.getName());
-        customerEntity.setEmail(customer.getEmail());
+    com.proto.service.Customer entityToCustomer(Customer entity);
 
-        return customerEntity;
-    }
-
-    public com.proto.service.Customer entityToCustomer(Customer entity) {
-        if ( entity == null ) {
-            return null;
-        }
-
-        com.proto.service.Customer.Builder customer = com.proto.service.Customer.newBuilder();
-
-        return customer.setId(entity.getId()).setName(entity.getName()).setEmail(entity.getEmail()).build();
-    }
-
-    public List<com.proto.service.Customer> entityListToCustomerList(List<Customer> entityList) {
-        if ( entityList == null ) {
-            return null;
-        }
-
-        List<com.proto.service.Customer> list = new ArrayList<com.proto.service.Customer>( entityList.size() );
-        for ( Customer customer : entityList ) {
-            list.add( entityToCustomer(customer) );
-        }
-
-        return list;
-    }
+    List<com.proto.service.Customer> entityListToCustomerList(List<Customer> entityList);
 
 }
--- src/main/java/com/mapper/ProductMapper.java
+++ src/main/java/com/mapper/ProductMapper.java
@@ -1,47 +1,15 @@
 package com.mapper;
 
 import com.domain.Product;
-import jakarta.enterprise.context.ApplicationScoped;
+import org.mapstruct.Mapper;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@ApplicationScoped
-public class ProductMapper {
-    public Product productToEntity(com.proto.service.Product product) {
-        if ( product == null ) {
-            return null;
-        }
+@Mapper(componentModel = "jakarta")
+public interface ProductMapper {
+    Product productToEntity(com.proto.service.Product product);
 
-        Product productEntity = new Product();
-        productEntity.setId(product.getId());
-        productEntity.setName(product.getName());
-        productEntity.setPrice(product.getPrice());
+    com.proto.service.Product entityToProduct(Product entity);
 
-        return productEntity;
-    }
-
-    public com.proto.service.Product entityToProduct(Product entity) {
-        if ( entity == null ) {
-            return null;
-        }
-
-        com.proto.service.Product.Builder product = com.proto.service.Product.newBuilder();
-
-        return product.setId(entity.getId()).setName(entity.getName()).setPrice(entity.getPrice()).build();
-    }
-
-
-    public List<com.proto.service.Product> entityListToProductList(List<Product> entityList) {
-        if ( entityList == null ) {
-            return null;
-        }
-
-        List<com.proto.service.Product> list = new ArrayList<com.proto.service.Product>( entityList.size() );
-        for ( Product product : entityList ) {
-            list.add( entityToProduct(product) );
-        }
-
-        return list;
-    }
+    List<com.proto.service.Product> entityListToProductList(List<Product> entityList);
 }
