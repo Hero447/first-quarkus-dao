894e85e91cb28adf7e32c8124647e1a4f7ed393d
Hero447 <sergsoldpp@gmail.com>
2026-03-12 17:26:05 +0200

    fix liquibase-reactive jdbc

--- src/main/java/com/repository/CustomerRepository.java
+++ src/main/java/com/repository/CustomerRepository.java
@@ -2,10 +2,8 @@ package com.repository;
 
 import com.domain.Customer;
 import io.quarkus.hibernate.reactive.panache.PanacheRepository;
-import io.smallrye.common.annotation.Blocking;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-@Blocking
 public class CustomerRepository implements PanacheRepository<Customer> {
 }
--- src/main/java/com/repository/ProductRepository.java
+++ src/main/java/com/repository/ProductRepository.java
@@ -2,10 +2,8 @@ package com.repository;
 
 import com.domain.Product;
 import io.quarkus.hibernate.reactive.panache.PanacheRepository;
-import io.smallrye.common.annotation.Blocking;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-@Blocking
 public class ProductRepository implements PanacheRepository<Product> {
 }
